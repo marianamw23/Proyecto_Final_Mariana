83b142f803efb3638aad78d55e327f9ab80f4ceb
mariana macias <mmacias82659@universidadean.edu.co>
2025-08-29 14:21:41 -0500

    29 agosto

--- mariana_macias.py
+++ mariana_macias.py
@@ -1,4 +1,29 @@
 # mariana_macias.py
+
 def presentar():
     print("hola,este es mi primer commit")
-    print("soy el mejor del mundo")
+
+# Lista de ingredientes riesgosos con sus puntajes negativos 
+
+ingredientes_riesgosos= {
+    "paraben ":-2,
+    "phenoxyethanol":-1,
+    "triclosan":-3,
+    "formaldedehyde":-5,
+    "dmdm hydantoin": -4,
+    "imidazolidinyl urea": -4,
+    "sodium laurylsulfate": -3, 
+    "sls": -3, 
+    "sles": -3, 
+    "alcohol denat": -2, 
+    "ethanol": -2,
+    "isopropyl alcohol": -2, 
+    "dimethicone": -1,
+    "cyclopentasiloxane": -1, 
+    "petrolatum": -2, 
+    "mineral oil": -2,
+    "fragrance": -2,
+    "parfum": -2,
+    "colorant": -1,
+    "fd&c": -1
+}
